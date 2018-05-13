3a0d36fbd5b5cb1ce30cf5fad8333863d22940f1
Rohit Macherla <rohit.macherla2@sky.uk>
2018-05-13 21:11:38 +0100

    Added background image, playing with content and competencies

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /
     path('', views.index, name='base'),
+    path('competencies/', views.competency_list, name='competency_list'),
     # ex: /competency/1
     path('competency/<int:competency_id>/', views.competency_detail,
          name='competency_detail')
--- base/views.py
+++ base/views.py
@@ -5,10 +5,15 @@ from .models import Competency
 
 def index(request):
     """Home page."""
+    return render(request, 'base/index.html', {})
+
+
+def competency_list(request):
+    """Home page."""
     competencies = Competency.objects.order_by('used_from_date')
     context = {'competencies': competencies}
 
-    return render(request, 'base/index.html', context)
+    return render(request, 'base/competency_list.html', context)
 
 
 def competency_detail(request, competency_id):
